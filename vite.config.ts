2a0c537644b395f311d1274f41a47d4ee8ca0f4d
mworkroom <easylong@gmail.com>
2026-07-06 09:03:11 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "./",
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        business: "index.html",
+        center: "center/index.html",
+        admin: "admin/index.html"
+      }
+    }
+  }
 });
